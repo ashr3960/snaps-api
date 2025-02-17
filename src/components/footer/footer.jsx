cb051269050b3f9551f2aa3a0cfc2c5e73c60f9d
Rehma Ashraf <12r.ashraf@gmail.com>
2025-02-17 07:40:38 -0500

    done with the footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./footer.css";
+import FacebookIcon from "../../assets/images/Facebook.svg";
+import TwitterIcon from "../../assets/images/X_twitter.svg";
+import InstagramIcon from "../../assets/images/Instagram.svg";
+import PinterestIcon from "../../assets/images/Pinterest.svg";
+
 
 function Footer() {
     return (
         <footer className="footer">
             <div className="container">
 
-                <div className="footer-left">
-                    <h2 className="logo">Snaps</h2>
-                </div>
+                <h2 className="title"> Snaps</h2>
 
                 <div className="footer-links">
                     <div className="footer-column">
@@ -25,19 +28,19 @@ function Footer() {
                 </div>
 
                 <div className="footer-social">
-                    <a href="#"><i className="fab fa-facebook"></i></a>
-                    <a href="#"><i className="fab fa-instagram"></i></a>
-                    <a href="#"><i className="fab fa-pinterest"></i></a>
+                    <a href="#"><img src={FacebookIcon} alt="Facebook" className="social-icon" /></a>
+                    <a href="#"><img src={TwitterIcon} alt="Twitter" className="social-icon" /></a>
+                    <a href="#"><img src={InstagramIcon} alt="Instagram" className="social-icon" /></a>
+                    <a href="#"><img src={PinterestIcon} alt="Pinterest" className="social-icon" /></a>
                 </div>
+
             </div>
 
             <div className="footer-bottom">
                 <p>© 2024 Snaps · <a href="#">Terms</a> <a href="#">Privacy</a> <a href="#">Cookies</a></p>
             </div>
-            
         </footer>
     );
 }
   
 export default Footer;
-  
