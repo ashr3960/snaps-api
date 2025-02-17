c5053f1549005fa46d104fa0fffe4266ec62e4b5
Rehma Ashraf <12r.ashraf@gmail.com>
2025-02-16 19:24:03 -0500

    did the navbar

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <div className="container">
+            <h1 className="title">Snaps</h1>
+            <div className="cta">
+                <p className="cta-title">Filters</p>
+                <FontAwesomeIcon icon={faBars} className="icon" />
+            </div>
+        </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
