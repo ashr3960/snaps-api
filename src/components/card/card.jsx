a6cde3973e775136378175ec21f776c3c3a236db
Rehma Ashraf <12r.ashraf@gmail.com>
2025-02-17 19:16:11 -0500

    done with card compoent

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,21 +1,23 @@
 import React from "react";
 import "./card.css";
 
-
-function Card({photo, photographer, tags}) {
+function Card({ photo, photographer, tags }) {
   return (
     <div className="card">
-        <div className="container">
-            <img src={photo} alt="Photograph" className="card__image" />
-            <p className="card__name">{photographer}</p>
-            <div className="card__tags">
-                {tags.map((tag, index) => (
-                <span key={index} className="card__tag">
-                    {tag}
-                </span>
-                ))}
-            </div>
-        </div>
+
+      <div className="image-container">
+        <img src={photo} alt="Photograph" className="card__image" />
+        <p className="card__name">{photographer}</p>
+      </div>
+
+      <div className="card__tags">
+        {tags.map((tag, index) => (
+          <span key={index} className="card__tag">
+            {tag}
+          </span>
+        ))}
+      </div>
+      
     </div>
   );
 }
